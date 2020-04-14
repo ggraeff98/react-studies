b5d55c5322df5290b2fca4c7d0a68fdefd46924b
Gustavo <gustavograeff1998@gmail.com>
2020-04-14 11:55:59 -0300

    add: componentes to app js

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,7 +1,22 @@
 import React from 'react';
+import Layout from './components/Layout/Layout';
+import styled from 'styled-components';
+import BurguerBuilder from './containers/BuguerBuilder/BurguerBuilder';
+
+const StyledAppDiv = styled.div`
+  margin: 0;
+  padding: 0;
+  font-family: 'Open Sans';
+`;
 
 const app = () => {
-  return <h1>Teste</h1>;
+  return (
+    <StyledAppDiv>
+      <Layout>
+        <BurguerBuilder></BurguerBuilder>
+      </Layout>
+    </StyledAppDiv>
+  );
 };
 
 export default app;
