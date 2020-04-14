9b57a1d6a309e2e2b31f786f241b59b841f20d74
Gustavo <gustavograeff1998@gmail.com>
2020-04-14 15:22:16 -0300

    add: firsts real states manipulations

--- my-app/src/containers/BuguerBuilder/BurguerBuilder.js
+++ my-app/src/containers/BuguerBuilder/BurguerBuilder.js
@@ -1,13 +1,32 @@
-import React from 'react';
+import React, { useState } from 'react';
 import Aux from '../../hoc/Aux';
+import Burguer from '../../components/Burguer/Burguer';
 
-const burguerBuilder = () => {
+const BurguerBuilder = () => {
+  const [ingredientsState, setIngridientsState] = useState({
+    ingredients: {
+      salad: 1,
+      bacon: 1,
+      cheese: 2,
+      meat: 2
+    }
+  });
+
+  const switchBurguerIngredients = (ingredient, amount) => {
+    const ingredients = ingredientsState.ingredients;
+    ingredients[`${ingredient}`] = amount;
+
+    setIngridientsState({
+      ingredients
+    });
+  };
+
   return (
     <Aux>
-      <div>Burguer</div>
-      <div>Build Controls</div>
+      <Burguer ingredients={ingredientsState.ingredients}></Burguer>
+      <div onClick={() => switchBurguerIngredients('salad', 0)}>Build Controls</div>
     </Aux>
   );
 };
 
-export default burguerBuilder;
+export default BurguerBuilder;
