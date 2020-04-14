65692cec4c7ce7a120fe958bbe09e54dc33aeb39
Gustavo <gustavograeff1998@gmail.com>
2020-04-14 11:55:42 -0300

    add: burguer and burguer builder components drafts

---
+++ my-app/src/containers/BuguerBuilder/BurguerBuilder.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import Aux from '../../hoc/Aux';
+
+const burguerBuilder = () => {
+  return (
+    <Aux>
+      <div>Burguer</div>
+      <div>Build Controls</div>
+    </Aux>
+  );
+};
+
+export default burguerBuilder;
