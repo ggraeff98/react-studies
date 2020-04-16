da0f1c9305aa4cf9bfcf2036f5caf30ffa92c8a2
Gustavo <gustavograeff1998@gmail.com>
2020-04-16 19:31:33 -0300

    fix: burguer builder new path and reset my-app folder

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,22 +1,7 @@
 import React from 'react';
-import Layout from './components/Layout/Layout';
-import styled from 'styled-components';
-import BurguerBuilder from './containers/BuguerBuilder/BurguerBuilder';
-
-const StyledAppDiv = styled.div`
-  margin: 0;
-  padding: 0;
-  font-family: 'Open Sans';
-`;
 
 const app = () => {
-  return (
-    <StyledAppDiv>
-      <Layout>
-        <BurguerBuilder></BurguerBuilder>
-      </Layout>
-    </StyledAppDiv>
-  );
+  return <div></div>;
 };
 
 export default app;
